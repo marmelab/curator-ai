e6fb54ba37f04aa988851190ad21f026e40320de
SLIMANI Theo <theo.slimani@telecomnancy.eu>
2024-10-24 11:12:49 +0200

    [refactor] fixed file name

--- src/script_curator/newsletter_script.ts
+++ src/script_curator/newsletter_script.ts
@@ -1,8 +1,10 @@
-import { curateAndGenerateNewsletter } from './newletter_txt_format';
+import { curateAndGenerateNewsletter } from './newsletter_txt_format';
 
 async function runNewsletter() {
     const newsletterText = await curateAndGenerateNewsletter();
     console.log(newsletterText);  // Affiche ou traite la newsletter ici
 }
+
+
 
 runNewsletter();
