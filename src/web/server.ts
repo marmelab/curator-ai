4d3c03a0697e514b301c1873bdac3c2c2ba369a8
Pierre Auguste <pierre.auguste4@etu.univ-lorraine.fr>
2024-10-25 09:45:57 +0200

    [feat] unsubscribe page

--- src/web/server.ts
+++ src/web/server.ts
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
-    res.redirect('/en/index.html'); 
+    res.redirect('/en/index.html');
 });
 
 // Route to handle email submission
@@ -44,22 +44,67 @@ app.post('/submit-email', async (req, res) => {
             }
         }
 
+        // In case of error, two res.status().json are send, that made the sever crash. To be fixed.
+
         console.log('Email added to Supabase:', data);
 
-    //     // Send the welcome email via Postmark (uncomment to enable email sending)
-    //     // await client.sendEmail({
-    //     //   From: 'your_verified_email@example.com', // Use a verified email address in Postmark
-    //     //   To: email,
-    //     //   Subject: 'Welcome Email',
-    //     //   TextBody: 'Thank you for signing up for our application!',
-    //     //   HtmlBody: '<strong>Thank you for signing up for our application!</strong>',
-    //     // });
+        //     // Send the welcome email via Postmark (uncomment to enable email sending)
+        //     // await client.sendEmail({
+        //     //   From: 'your_verified_email@example.com', // Use a verified email address in Postmark
+        //     //   To: email,
+        //     //   Subject: 'Welcome Email',
+        //     //   TextBody: 'Thank you for signing up for our application!',
+        //     //   HtmlBody: '<strong>Thank you for signing up for our application!</strong>',
+        //     // });
 
         res.status(200).json({ message: `A welcome email has been sent to ${email} and the address has been added to the database!` });
-        
+
     } catch (error) {
         console.error('Error processing the request:', error);
         res.status(500).json({ message: 'Error during registration. Please try again later.' });
+    }
+});
+
+// Route to handle email deletion
+app.post('/delete-email', async (req, res) => {
+    const email: string = req.body.email;
+
+    try {
+        // Remove the email to the Supabase database
+        const { data, error } = await supabase
+            .from('subscribers')
+            .delete()
+            .eq('email', { email })
+
+        if (error) {
+            if (error.code === '23503') {
+                res.status(400).json({ message: 'Cannot delete the record because it is referenced by another table.' });
+                /*} else if (data && data.length === 0) {
+                    // No rows were deleted
+                    res.status(404).json({ message: `No user found with email ${email}.` });*/
+            } else {
+                res.status(500).json({ message: 'Error during deletion. Please try again later.' });
+            }
+        }
+
+        // In case of error, two res.status().json are send, that made the sever crash. To be fixed.
+
+        console.log('Email removed :', data);
+
+        //     // Send the welcome email via Postmark (uncomment to enable email sending)
+        //     // await client.sendEmail({
+        //     //   From: 'your_verified_email@example.com', // Use a verified email address in Postmark
+        //     //   To: email,
+        //     //   Subject: 'Welcome Email',
+        //     //   TextBody: 'Thank you for signing up for our application!',
+        //     //   HtmlBody: '<strong>Thank you for signing up for our application!</strong>',
+        //     // });
+
+        res.status(200).json({ message: `The email ${email} and all the preferences associated have been removed from the database !` });
+
+    } catch (error) {
+        console.error('Error processing the request:', error);
+        res.status(500).json({ message: 'Error during deletion. Please try again later.' });
     }
 });
 
