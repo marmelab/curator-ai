b07d71b4489e6e89a705c2b80ca1401072b90e9a
Aurelien Gindre <aurelien.gindre@gmail.com>
2024-12-05 10:53:34 +0100

    [feat] Added form to subscribe on main page

--- website/src/components/Introduction.tsx
+++ website/src/components/Introduction.tsx
@@ -1,4 +1,5 @@
 import { Container } from '@/components/Container'
+import { Button } from '@/components/Button'
 
 export function Introduction() {
   return (
@@ -23,7 +24,34 @@ export function Introduction() {
         <p className="mt-2 font-semibold">
           CURATOR AI is made for you
         </p>        
+        <form className='mt-10 px-20'>
+          <h3 className="text-lg font-medium tracking-tight text-black">
+            Try it now !
+          </h3>
+          <div className="mt-4 sm:relative sm:flex sm:items-center sm:py-0.5 sm:pr-2.5">
+            <div className="relative sm:static sm:flex-auto">
+              <input
+                type="email"
+                id="email-address"
+                required
+                aria-label="Email address"
+                placeholder="Email address"
+                className="peer relative z-10 w-full appearance-none bg-transparent px-4 py-2 text-base text-black placeholder:text-slate-400/70 focus:outline-none sm:py-3"
+              />
+              <div className="absolute inset-0 rounded-md border border-black/40  peer-focus:border-black  peer-focus:ring-1 peer-focus:ring-blue-300 sm:rounded-xl" />
+            </div>
+            <Button
+              type="submit"
+              variant='solid'
+              color="blue"
+              className="mt-4 w-full sm:relative sm:z-10 sm:mt-0 sm:w-auto sm:flex-none"
+            >
+              Subscribe
+            </Button>
+          </div>
+        </form>
       </Container>
+      
     </section>
   )
 }
