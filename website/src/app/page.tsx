f2a1b3dfdb7b23201774a9ce8d07ff56ec508d8e
SLIMANI Theo <theo.slimani@telecomnancy.eu>
2024-12-05 14:47:35 +0100

    setup supabase

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,7 +1,15 @@
+import { createClient } from '@supabase/supabase-js'
+
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Introduction } from '@/components/Introduction'
 import { PersonalizedSection } from '@/components/PersonalizedSection'
+
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+);
 
 export default function Home() {
   return (
