5a9b695356dae85b63f645a70813f1cc3fc294e0
Pierre Auguste <pierre.auguste@telecomnancy.net>
2024-12-07 10:41:58 +0100

    [feat] adding i18n to the prject

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,6 +1,8 @@
+'use client'
 import { Features } from '@/components/Features'
 import { Footer } from '@/components/Footer'
 import { Hero } from '@/components/Hero'
+import '@/i18n';  // Import the i18n configuration
 
 export default function Home() {
   return (
