93c53cf5f3135ca0b67c9088273f1cda4df108b3
Pierre Auguste <pierre.auguste@telecomnancy.net>
2024-12-07 10:41:58 +0100

    [feat] adding i18n to the prject

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,9 +1,11 @@
+'use client'
 import { Features } from '@/components/Features'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
 import { Introduction } from '@/components/Introduction'
 import { PersonalizedSection } from '@/components/PersonalizedSection'
+import '@/i18n';  // Import the i18n configuration
 
 export default function Home() {
   return (
--- website/src/components/PersonalizedSection.tsx
+++ website/src/components/PersonalizedSection.tsx
@@ -3,8 +3,11 @@ import Image from 'next/image'
 import { CheckIcon } from '@/components/CheckIcon'
 import { Container } from '@/components/Container'
 import SampleImage from '@/images/SampleImage.png'
+// import the i18n configuration
+import { useTranslation } from 'react-i18next';
 
 export function PersonalizedSection() {
+  const { t } = useTranslation();  // `t` is the function to access translations
   return (
     <section
       id="free-chapters"
@@ -18,32 +21,32 @@ export function PersonalizedSection() {
         >
           <div className="flex flex-col justify-center">
             <h3 className="font-display text-base font-extrabold tracking-tight text-mid_blue sm:w-3/4 md:w-2/3 lg:w-auto">
-              Customizable
+              {t('person0')}
             </h3>
             <h2 className="font-display text-4xl font-extrabold tracking-tight text-black sm:w-3/4 md:w-2/3 lg:w-auto">
-              Your personalized news curator
+              {t('person1')}
             </h2>
             <p className="mt-4 text-lg tracking-tight text-black">
-              Set your email preferences and receive a newsletter that <b>suits you!</b>
+              {t('person2')}
             </p>
             <ul role="list" className="mt-8 space-y-3">
-            {[
-                'Choose the piece of news you want to follow',
-                'Set when you want to reveive your news : daily, weekly, on fridays...',
-            ].map((feature) => (
+              {[
+                t('person3'),
+                t('person4')
+              ].map((feature) => (
                 <li key={feature} className="flex">
-                <CheckIcon className="h-8 w-8 flex-none fill-blue-500" />
-                <span className="ml-4">{feature}</span>
+                  <CheckIcon className="h-8 w-8 flex-none fill-blue-500" />
+                  <span className="ml-4">{feature}</span>
                 </li>
-            ))}
+              ))}
             </ul>
           </div>
-          
+
           <div className="flex justify-center lg:justify-end">
-            <Image 
-                className="max-w-md w-full h-auto object-cover"
-                src={SampleImage} 
-                alt="Picture of a sample email. Each news has a title and a brief descritpion made by the AI"
+            <Image
+              className="max-w-md w-full h-auto object-cover"
+              src={SampleImage}
+              alt="Picture of a sample email. Each news has a title and a brief descritpion made by the AI"
             />
           </div>
         </Container>
