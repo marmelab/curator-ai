03847e0ebde7b00e45978105f2929585b48d9511
Aurelien Gindre <aurelien.gindre@gmail.com>
2024-12-10 09:42:06 +0100

    [chore] Remove unused components

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,9 +1,6 @@
 import { Features } from '@/components/Features'
 import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
-import { Introduction } from '@/components/Introduction'
-import { PersonalizedSection } from '@/components/PersonalizedSection'
 
 export default function Home() {
   return (
