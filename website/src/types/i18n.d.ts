9a3e136526d2e2d36383c5890fa8efad6202c2ba
Pierre Auguste <pierre.auguste4@etu.univ-lorraine.fr>
2024-12-11 10:31:52 +0100

    [chore] Correcting comments

--- website/src/types/i18n.d.ts
+++ website/src/types/i18n.d.ts
@@ -1,8 +1,7 @@
-// src/types/i18n.d.ts
 import 'i18next';
 
 declare module 'i18next' {
     interface i18n {
-        use: (plugin: any) => i18n; // Extend the `use` method
+        use: (plugin: any) => i18n;
     }
 }
