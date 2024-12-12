4db4680832a26ef663e22e7fd42a28ac3f64c490
louis.ferry@telecomnancy.eu <louis.ferry@telecomnancy.eu>
2024-12-12 09:40:16 +0100

    Refactor and translation of comments

--- website/src/components/Features.tsx
+++ website/src/components/Features.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { CheckIcon } from './CheckIcon';
 import Image from 'next/image';
 import sampleImage from '@/images/sampleImage.png';
-import {Button} from '@/components/Button';
+import { Button } from '@/components/Button';
 
 export function Features() {
   const [email, setEmail] = useState('');
@@ -96,35 +96,35 @@ export function Features() {
               ))}
             </ul>
             {/* Formulaire de saisie de l'email */}
-        <form className="mt-10 px-20" onSubmit={handleEmailSubmission}>
-          <h3 className="text-lg font-medium tracking-tight text-black">Try it now !</h3>
-          <div className="mt-4 sm:relative sm:flex sm:items-center sm:py-0.5 sm:pr-2.5">
-            <div className="relative sm:static sm:flex-auto">
-              <input
-                type="email"
-                id="email-address"
-                required
-                aria-label="Email address"
-                placeholder="Email address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)} // Mettez à jour l'email dans l'état
-                className="peer relative z-10 w-full appearance-none bg-transparent px-4 py-2 text-base text-black placeholder:text-slate-400/70 focus:outline-none sm:py-3"
-              />
-              <div className="absolute inset-0 rounded-md border border-black/40 peer-focus:border-black peer-focus:ring-1 peer-focus:ring-blue-300 sm:rounded-xl" />
-            </div>
-            <Button
-              type="submit"
-              variant="solid"
-              color="blue"
-              className="mt-4 w-full sm:relative sm:z-10 sm:mt-0 sm:w-auto sm:flex-none"
-            >
-              Subscribe
-            </Button>
-          </div>
-        </form>
+            <form className="mt-10 px-20" onSubmit={handleEmailSubmission}>
+              <h3 className="text-lg font-medium tracking-tight text-black">Try it now !</h3>
+              <div className="mt-4 sm:relative sm:flex sm:items-center sm:py-0.5 sm:pr-2.5">
+                <div className="relative sm:static sm:flex-auto">
+                  <input
+                    type="email"
+                    id="email-address"
+                    required
+                    aria-label="Email address"
+                    placeholder="Email address"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} // Mettez à jour l'email dans l'état
+                    className="peer relative z-10 w-full appearance-none bg-transparent px-4 py-2 text-base text-black placeholder:text-slate-400/70 focus:outline-none sm:py-3"
+                  />
+                  <div className="absolute inset-0 rounded-md border border-black/40 peer-focus:border-black peer-focus:ring-1 peer-focus:ring-blue-300 sm:rounded-xl" />
+                </div>
+                <Button
+                  type="submit"
+                  variant="solid"
+                  color="blue"
+                  className="mt-4 w-full sm:relative sm:z-10 sm:mt-0 sm:w-auto sm:flex-none"
+                >
+                  Subscribe
+                </Button>
+              </div>
+            </form>
 
-        {/* Affichage du message de confirmation */}
-        {message && <p className="mt-4 text-green-500">{message}</p>}
+            {/* Affichage du message de confirmation */}
+            {message && <p className="mt-4 text-green-500">{message}</p>}
           </div>
         </div>
         <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
