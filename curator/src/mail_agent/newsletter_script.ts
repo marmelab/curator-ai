faa90b811226858b00f03b19f90dc9f9ce874ad6
SLIMANI Theo <theo.slimani@telecomnancy.eu>
2024-12-12 16:48:49 +0100

    [chore] fix based on PR comments

--- curator/src/mail_agent/newsletter_script.ts
+++ curator/src/mail_agent/newsletter_script.ts
@@ -1,31 +1,43 @@
 import { curateAndGenerateNewsletter } from './newsletter_txt_format';
 import dotenv from 'dotenv';
+import { ServerClient } from 'postmark';
 
 dotenv.config();
 
-var postmark = require("postmark");
+const send_mail = true;
 
 // Calls the curateAndGenerateNewsletter function with right parameters then sends the mail
 // TODO : Add all the dynamic part (newsletter parameters, email params)
 
 async function runNewsletter() {
-    const {markdown, html} = await curateAndGenerateNewsletter();
-    //console.log(markdown);  // uncomment this part and comment below to test without postmark
+  const {markdown, html} = await curateAndGenerateNewsletter();
 
-    // Sending email :
-    var client = new postmark.ServerClient(process.env.POSTMARK_SERVER_API_KEY as string);
-    var mail = process.env.POSTMARK_DEFAULT_MAIL as string;
+  // Sending email :
+  if (process.env.POSTMARK_SERVER_API_KEY && process.env.POSTMARK_DEFAULT_MAIL && send_mail) {
+
+    const client = new ServerClient(process.env.POSTMARK_SERVER_API_KEY as string);
+    const mail = process.env.POSTMARK_DEFAULT_MAIL as string;
 
     client.sendEmail({
-    "From": mail,
-    "To": mail,
-    "Subject": "Your weekly newsletter",
-    "HtmlBody": html,
-    "TextBody": markdown,
-    "MessageStream": "outbound"
+      'From': mail,
+      'To': mail,
+      'Subject': 'Your weekly newsletter',
+      'HtmlBody': html,
+      'TextBody': markdown,
+      'MessageStream': 'outbound'
     });
-    console.log("Newsletter email sent")
 
+    console.log('Newsletter email sent');
+  }
+  else {
+
+    if (!(process.env.POSTMARK_SERVER_API_KEY) || !(process.env.POSTMARK_DEFAULT_MAIL)) {
+      console.error('Make sure to define POSTMARK_SERVER_API_KEY and POSTMARK_DEFAULT_MAIL if you want to send mail');
+    }
+
+    console.log('Here is the current result :');
+    console.log(markdown);
+  }
 }
 
 runNewsletter();
--- curator/src/mail_agent/newsletter_txt_format.ts
+++ curator/src/mail_agent/newsletter_txt_format.ts
@@ -1,15 +1,15 @@
-import { curate } from "../curate";
+import { curate } from '../curate';
 
-import { Summary } from "../types";
+import { Summary } from '../types';
 
 // links to curate from (TODO : make it based on user preference)
 
 const links = [
-    'https://www.fromjason.xyz/p/notebook/where-have-all-the-websites-gone/',
-    'https://www.joelonsoftware.com/2000/04/06/things-you-should-never-do-part-i/',
-    'https://biomejs.dev/blog/biome-v1-5/',
-    'https://birtles.blog/2024/01/06/weird-things-engineers-believe-about-development/',
-    'https://julesblom.com/writing/flushsync',
+  'https://www.fromjason.xyz/p/notebook/where-have-all-the-websites-gone/',
+  'https://www.joelonsoftware.com/2000/04/06/things-you-should-never-do-part-i/',
+  'https://biomejs.dev/blog/biome-v1-5/',
+  'https://birtles.blog/2024/01/06/weird-things-engineers-believe-about-development/',
+  'https://julesblom.com/writing/flushsync',
 ];
 
 // Formats the newsletter in markdown 
@@ -19,31 +19,31 @@ const links = [
 
 function formatNewsletterMarkdown(articles : Summary[]) {
 
-    // Newletter's header
+  // Newletter's header
 
-    let newsletter = 'Newsletter MARKDOWN\n\n';
-    newsletter += "Hello everyone! Here are the latest news!\n\n";
+  let newsletter = 'Newsletter\n\n';
+  newsletter += 'Hello everyone! Here are the latest news!\n\n';
 
-    // Formatting for each article
+  // Formatting for each article
 
-    articles.forEach(article => {
-        const title = article.title || "Title not available";
-        const author = article.author || "Unknown author";
-        const summary = article.summary || "Summary not available.";
-        const link = article.link || "#";
+  articles.forEach(article => {
+    const title = article.title || 'Title not available';
+    const author = article.author || 'Unknown author';
+    const summary = article.summary || 'Summary not available.';
+    const link = article.link || '#';
 
         
-        newsletter += `### ${title}\n`;
-        newsletter += `*by ${author}*\n`;
-        newsletter += `🔗 [Read the full article](${link})\n\n`;
-        newsletter += `> ${summary}\n\n`;
-    });
+    newsletter += `### ${title}\n`;
+    newsletter += `*by ${author}*\n`;
+    newsletter += `🔗 [Read the full article](${link})\n\n`;
+    newsletter += `> ${summary}\n\n`;
+  });
 
-    // Footer
+  // Footer
 
-    newsletter += "\nThat's all for now! See you soon for more news!\n";
+  newsletter += '\nThat\'s all for now! See you soon for more news!\n';
 
-    return newsletter;
+  return newsletter;
 }
 
 // Formats the newsletter in html with style
@@ -53,23 +53,23 @@ function formatNewsletterMarkdown(articles : Summary[]) {
 
 function formatNewsletterHtmlWithCSS(articles: Summary[]) {
 
-    // Header
+  // Header
 
-    let htmlNewsletter = `
+  let htmlNewsletter = `
     <div style="font-family: Arial, sans-serif; background-color: #f9f9f9; color: #333; padding: 20px; border-radius: 10px; max-width: 800px; margin: 0 auto;">
         <h1 style="color: #4CAF50; text-align: center; font-size: 32px;">Newsletter</h1>
         <p style="font-size: 18px; text-align: center;">Hello everyone! Here are the latest news!</p>
     `;
 
-    // Formatting each article
+  // Formatting each article
 
-    articles.forEach(article => {
-        const title = article.title || "Title not available";
-        const author = article.author || "Unknown author";
-        const summary = article.summary || "Summary not available.";
-        const link = article.link || "#";
+  articles.forEach(article => {
+    const title = article.title || 'Title not available';
+    const author = article.author || 'Unknown author';
+    const summary = article.summary || 'Summary not available.';
+    const link = article.link || '#';
 
-        htmlNewsletter += `
+    htmlNewsletter += `
         <div style="margin-bottom: 30px; padding: 20px; background-color: #fff; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
             <h3 style="color: #FF5722; font-size: 24px;">${title}</h3>
             <p style="font-size: 16px; font-weight: bold; color: #555;">by ${author}</p>
@@ -77,10 +77,10 @@ function formatNewsletterHtmlWithCSS(articles: Summary[]) {
             <blockquote style="font-size: 16px; color: #777; border-left: 4px solid #ddd; padding-left: 15px;">${summary}</blockquote>
         </div>
         `;
-    });
+  });
 
-    htmlNewsletter += '</div>';
-    return htmlNewsletter;
+  htmlNewsletter += '</div>';
+  return htmlNewsletter;
 }
 
 // Uses the curate function to generate newsletter data and formats output to be used in mail
@@ -88,30 +88,30 @@ function formatNewsletterHtmlWithCSS(articles: Summary[]) {
 
 export function curateAndGenerateNewsletter(): Promise<{markdown: string, html: string }> {
     
-    return curate({
-        links,
-        interests: ['react', 'ai'],
-        max: 5,
-    }).then((curatedLinks: Summary[]) => {
+  return curate({
+    links,
+    interests: ['react', 'ai'],
+    max: 5,
+  }).then((curatedLinks: Summary[]) => {
 
-        // Generate the formatted string when promise completed
+    // Generate the formatted string when promise completed
 
-        const markdown = formatNewsletterMarkdown(curatedLinks);
-        const html = formatNewsletterHtmlWithCSS(curatedLinks);
+    const markdown = formatNewsletterMarkdown(curatedLinks);
+    const html = formatNewsletterHtmlWithCSS(curatedLinks);
 
-        // Returns raw json and formatted newletters
+    // Returns raw json and formatted newletters
 
-        return { markdown, html};
+    return { markdown, html};
 
-    }).catch((err: unknown) => {
+  }).catch((err: unknown) => {
 
-        // Will most likely be instance of Error TODO : check if there are different error cases
+    // Will most likely be instance of Error TODO : check if there are different error cases
 
-        if (err instanceof Error) {
-            console.error("Error during link curation: ", err.message);
-        } else {
-            console.error("Unknown error occurred during link curation");
-        }
-        return { markdown: '', html: '' }
-    });
+    if (err instanceof Error) {
+      console.error('Error during link curation: ', err.message);
+    } else {
+      console.error('Unknown error occurred during link curation');
+    }
+    return { markdown: '', html: '' };
+  });
 }
