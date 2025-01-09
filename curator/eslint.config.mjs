c8a24d2ab213abf0fe7f99b5dc09100ed2a33dd0
SLIMANI Theo <theo.slimani@telecomnancy.eu>
2025-01-09 10:32:20 +0100

    [chore] removed package lock in curator

--- curator/eslint.config.mjs
+++ curator/eslint.config.mjs
@@ -7,26 +7,26 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+    {
+        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+        languageOptions: {
+            globals: { ...globals.browser, ...globals.node },
+        },
+        plugins: {
+            react: pluginReact,
+            'react-hooks': pluginReactHooks,
+        },
+        rules: {
+            quotes: ['error', 'single'],
+            semi: ['error', 'always'],
+            indent: ['error', 2],
+            'no-multi-spaces': ['error'],
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+        },
     },
-    plugins: {
-      react: pluginReact,
-      'react-hooks': pluginReactHooks,
-    },
-    rules: {
-      quotes: ['error', 'single'],
-      semi: ['error', 'always'],
-      indent: ['error', 2],
-      'no-multi-spaces': ['error'],
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-    },
-  },
-  pluginJs.configs.recommended,
-  ...tsEsLint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  eslintConfigPrettier
+    pluginJs.configs.recommended,
+    ...tsEsLint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    eslintConfigPrettier,
 ];
