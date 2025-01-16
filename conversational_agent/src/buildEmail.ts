c04505594ce620f2c611427062d2ecaf3a9099a7
Pierre Auguste <pierre.auguste4@etu.univ-lorraine.fr>
2025-01-16 15:47:04 +0100

    [refactor] Suppressing unecessary line

--- conversational_agent/src/buildEmail.ts
+++ conversational_agent/src/buildEmail.ts
@@ -4,8 +4,6 @@ export const buildResponse = async (body: any) => {
     // Generate a response from AI based on the received email text
     const aiResponse = await getUserPreferences(body['TextBody']);
 
-    let response = `Hello!\n\n`;
-
     const emailMetadata = `
         
         -------- Previous Message --------
