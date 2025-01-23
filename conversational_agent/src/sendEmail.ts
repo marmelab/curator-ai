1fa72039e75e158783735f31a121c63847054c89
Pierre Auguste <pierre.auguste4@etu.univ-lorraine.fr>
2025-01-23 11:18:42 +0100

    [chore] merging some code

--- conversational_agent/src/sendEmail.ts
+++ conversational_agent/src/sendEmail.ts
@@ -1,27 +1,68 @@
 import dotenv from 'dotenv';
 import { ServerClient } from 'postmark';
+import { getUserPreferences } from './getUserPreferences';
 
 // Load environment variables from the .env file
 dotenv.config({ path: './../.env' });
 
-export const sendMail = async (to: string, subject: string, body: string) => {
+export const sendMail = async (body: any) => {
     // Use the Postmark API key from environment variables
     const client = new ServerClient(process.env.POSTMARK_API_KEY || '');
 
     try {
+        const formattedBody = await buildResponse(body);
         // Send an email
         const result = await client.sendEmail({
             From: process.env.DEFAULT_POSTMARK_MAIL || '', // Replace with a verified email
-            To: to,
-            Subject: 'Re: ' + subject,
-            HtmlBody: formatHtmlBody(body),
-            TextBody: formatTextBody(body),
+            To: body['From'],
+            Subject: 'Re: ' + body['Subject'],
+            HtmlBody: formatHtmlBody(formattedBody),
+            TextBody: formatTextBody(formattedBody),
             MessageStream: 'outbound',
         });
         console.error('E-Mail sent successfully : ', result);
     } catch (error) {
         console.error('Error when trying to send the E-Mail :', error);
     }
+};
+
+const buildResponse = async (body: any) => {
+    // Generate a response from AI based on the received email text
+    const aiResponse = await getUserPreferences(body['TextBody']);
+
+    const emailMetadata = `
+        
+        -------- Previous Message --------
+        
+        From: ${body['From']}
+        
+        Sent: ${body['Date']}
+        
+        To: ${body['To']}
+        
+        Subject: ${body['Subject']}
+        
+        ${body['TextBody']}
+    
+    `;
+
+    if (!aiResponse?.themes?.length && !aiResponse?.unwanted_themes?.length) {
+        return `Sorry, we didn't find any preferences in your E-Mail. ${emailMetadata}`;
+    }
+
+    let response = `Hello!\n`;
+    if (aiResponse?.themes?.length) {
+        response += `The following ${aiResponse?.themes.length == 1 ? 'theme' : 'themes'} have been added to your next newsletters :
+  - ${aiResponse?.themes.join('\n  - ')}`;
+    }
+
+    if (aiResponse?.unwanted_themes?.length) {
+        response += `\nYou will no longer be annoyed with the following ${aiResponse?.themes.length == 1 ? 'theme' : 'themes'} :
+  - ${aiResponse?.unwanted_themes.join('\n  - ')}`;
+    }
+
+    return `${response}
+        ${emailMetadata}`;
 };
 
 /**
