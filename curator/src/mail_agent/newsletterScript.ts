d29e6e02ecf94eea9e2eef7570565feac00cec06
louis.ferry@telecomnancy.eu <louis.ferry@telecomnancy.eu>
2025-02-03 10:20:03 +0100

    fix : early return

--- curator/src/mail_agent/newsletterScript.ts
+++ curator/src/mail_agent/newsletterScript.ts
@@ -12,38 +12,36 @@ const sendMail = true;
 async function runNewsletter() {
     const { markdown, html } = await curateAndGenerateNewsletter();
 
-    // Sending email :
     if (
-        process.env.POSTMARK_API_KEY &&
-        process.env.DEFAULT_POSTMARK_EMAIL &&
-        sendMail
-    ) {
-        const client = new ServerClient(process.env.POSTMARK_API_KEY as string);
-        const mail = process.env.DEFAULT_POSTMARK_EMAIL as string;
+        !process.env.POSTMARK_API_KEY ||
+        !process.env.DEFAULT_POSTMARK_EMAIL
+    ){
+        throw new Error(
+            'Make sure to define POSTMARK_SERVER_API_KEY and POSTMARK_DEFAULT_MAIL if you want to send mail'
+        );
+    }
 
-        client.sendEmail({
-            From: mail,
-            To: mail,
-            Subject: 'Your weekly newsletter',
-            HtmlBody: html,
-            TextBody: markdown,
-            MessageStream: 'outbound',
-        });
-
-        console.log('Newsletter email sent');
-    } else {
-        if (
-            !process.env.POSTMARK_API_KEY ||
-            !process.env.DEFAULT_POSTMARK_EMAIL
-        ) {
-            console.error(
-                'Make sure to define POSTMARK_API_KEY and DEFAULT_POSTMARK_EMAIL if you want to send mail'
-            );
-        }
-
+    if (!sendMail){
         console.log('Here is the current result :');
         console.log(markdown);
+        return;
     }
-}
 
+
+    // Sending email :
+    const client = new ServerClient(process.env.POSTMARK_API_KEY as string);
+
+    const mail = process.env.DEFAULT_POSTMARK_EMAIL as string;
+
+    client.sendEmail({
+        From: mail,
+        To: mail,
+        Subject: 'Your weekly newsletter',
+        HtmlBody: html,
+        TextBody: markdown,
+        MessageStream: 'outbound',
+    });
+
+    console.log('Newsletter email sent')
+}
 runNewsletter();
