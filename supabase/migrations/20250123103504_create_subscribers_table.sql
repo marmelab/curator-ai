229c7b291bb2bc496e0402935d190318ae3de9b7
louis.ferry@telecomnancy.eu <louis.ferry@telecomnancy.eu>
2025-02-05 14:24:19 +0100

    fix : rename + fixing bug

--- supabase/migrations/20250123103504_create_subscribers_table.sql
+++ supabase/migrations/20250123103504_create_subscribers_table.sql
@@ -1,6 +1,8 @@
--- creation of the table subscribers
+-- Creation of the Subscribers table
 CREATE TABLE IF NOT EXISTS Subscribers (
-    id SERIAL PRIMARY KEY, -- unique id, auto-incremented
-    user_email VARCHAR(255) UNIQUE NOT NULL, -- unique email of the user, mendatory
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- date of subscription, default : nowaday
+    id SERIAL PRIMARY KEY, -- Unique id, auto-incremented
+    user_email VARCHAR(255) NOT NULL, -- Email of the user (remove UNIQUE if duplicate entries are allowed)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Date of subscription (defaults to current timestamp)
+    next_newsletter TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Next newsletter send date (defaults to now)
+    periodicity INTEGER DEFAULT 604800 -- Interval between newsletters in seconds (default: 604800 seconds = 7 days)
 );
