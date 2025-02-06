15ad6c79ab3cd147f67d420a8b9009ef59a1f70f
Pierre Auguste <pierre.auguste4@etu.univ-lorraine.fr>
2025-02-06 15:49:51 +0100

    [chore] refactiring some code for the db

--- conversational_agent/src/getUserPreferences.ts
+++ conversational_agent/src/getUserPreferences.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { zodResponseFormat } from 'openai/helpers/zod';
 import dotenv from 'dotenv';
 import { OpenAI } from 'openai';
-import { addSources, addThemes } from './savePreferences';
+import { addPreferences } from './savePreferences';
 
 dotenv.config({ path: './../.env' });
 
@@ -55,16 +55,13 @@ Ensure no duplicate entries in either "sources" or "unwanted_sources".
 
     const preferencesCompletion = completion.choices[0].message.parsed;
 
-    await addThemes(
-                preferencesCompletion?.themes || [],
-                preferencesCompletion?.unwantedThemes || [],
-                userMail
-            );
-    await addSources(
-                preferencesCompletion?.sources || [],
-                preferencesCompletion?.unwantedSources || [],
-                userMail
-            );
+    await addPreferences(
+        preferencesCompletion?.themes || [],
+        preferencesCompletion?.unwantedThemes || [],
+        preferencesCompletion?.sources || [],
+        preferencesCompletion?.unwantedSources || [],
+        userMail
+    );
 
     return preferencesCompletion;
 }
